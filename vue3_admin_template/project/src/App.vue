b1e784ce2dd567e0ae48b1273207d445156310e6
wang_cheng <wang_cheng@cestc.cn>
2024-01-08 16:34:05 +0800

    api统一接口管理

--- vue3_admin_template/project/src/App.vue
+++ vue3_admin_template/project/src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div>
-    <h1>测试axios二次封装</h1>
+    <h1>我是App根组件</h1>
   </div>
 </template>
 <script setup lang="ts" name="">
-import request from '@/utils/request'
-import { onMounted } from 'vue'
+// import { onMounted } from 'vue'
+// import { reqLogin } from './api/user'
 
-// 当组件挂载完毕，发送一个请求
-onMounted(() => {
-  request({
-    url: '/user/login',
-    method: 'post',
-    data: {
-      username: 'admin',
-      password: '111111',
-    },
-  }).then((res) => {
-    console.log(res)
-  })
-})
+// onMounted(() => {
+//   reqLogin({
+//     username: 'admin',
+//     password: '111111',
+//   })
+// })
 </script>
-<style scoped lang="scss">
-div {
-  h1 {
-    color: red;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
