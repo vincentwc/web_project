4275b7ac45e0332714f389f8281a913764967da3
wang_cheng <wang_cheng@cestc.cn>
2024-01-11 17:20:57 +0800

    提交

---
+++ vue3_admin_template/project/src/layout/main/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="">
+    <!-- 路由组件出口的位置 -->
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <!-- 渲染layout一级路由组件的子路由 -->
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+</template>
+<script setup lang="ts" name=""></script>
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+  transform: scale(0);
+}
+.fade-enter-active {
+  transition: all 0.3s;
+}
+.fade-enter-to {
+  transform: scale(1);
+}
+</style>
--- vue3_admin_template/project/src/views/acl/user/index.vue
+++ vue3_admin_template/project/src/views/acl/user/index.vue
@@ -1,9 +1,13 @@
 <template>
- <div class=''>
-  <h1>用户管理</h1>
- </div>
+  <div class="box">
+    <h1>用户管理</h1>
+  </div>
 </template>
-<script setup lang='ts' name=''>
-</script>
+<script setup lang="ts" name=""></script>
 <style scoped>
+.box {
+  width: 100%;
+  height: 400px;
+  background: red;
+}
 </style>
