16a0b21e62ba2a34afd4308bb62c45a7a691d6da
wang_cheng <wang_cheng@cestc.cn>
2024-01-14 16:19:41 +0800

    品牌数据展示

---
+++ vue3_admin_template/project/src/api/product/trademark/index.ts
@@ -0,0 +1,16 @@
+// 书写品牌管理模块接口
+import request from '@/utils/request'
+import type { TradeMarkResponseData } from './type'
+// 品牌管理模块接口地址
+enum API {
+  // 获取已有的品牌接口
+  TRADEMARK_URL = '/admin/product/baseTrademark/',
+}
+
+// 获取已有品牌接口的接口方法
+// page:获取第几页的数据--默认第一页
+// limit：获取几个已有的数据
+export const reqHasTrademark = (page: number, limit: number) =>
+  request.get<any, TradeMarkResponseData>(
+    API.TRADEMARK_URL + `${page}/${limit}`,
+  )
--- vue3_admin_template/project/src/views/product/trademark/index.vue
+++ vue3_admin_template/project/src/views/product/trademark/index.vue
@@ -14,15 +14,26 @@
         ---width:设置这列宽度
         ---align:设置这一列的对齐方式
        -->
-      <el-table style="margin: 10px 0px" border>
+      <el-table style="margin: 10px 0px" border :data="tradenarkArr">
         <el-table-column
           label="序号"
           width="80px"
+          type="index"
           align="center"
         ></el-table-column>
-        <el-table-column label="品牌名称"></el-table-column>
-        <el-table-column label="品牌LOGO"></el-table-column>
-        <el-table-column label="品牌操作"></el-table-column>
+        <!-- tabele-column:默认展示数据用div -->
+        <el-table-column label="品牌名称" prop="tmName"></el-table-column>
+        <el-table-column label="品牌LOGO" prop="logoUrl">
+          <template #="{ row }">
+            <img :src="row.logoUrl" style="width: 100px; height: 100px" />
+          </template>
+        </el-table-column>
+        <el-table-column label="品牌操作">
+          <template #="{ row, $index }">
+            <el-button type="warning" icon="Edit" size="small" />
+            <el-button type="danger" icon="Delete" size="small" />
+          </template>
+        </el-table-column>
       </el-table>
 
       <!-- 分页器组件 -->
@@ -35,21 +46,63 @@
           layout 可以设置分页器6个子组件布局调整
        -->
       <el-pagination
+        @current-change="getHasTrademark"
+        @size-change="sizeChange"
         v-model:current-page="pageNo"
-        v-model:page-size="pageSize"
+        v-model:page-size="limit"
         :page-sizes="[3, 5, 7, 9]"
         :background="true"
         layout=" prev, pager, next, jumper, ->,sizes,total"
-        :total="400"
+        :total="total"
       />
     </el-card>
   </div>
 </template>
 <script setup lang="ts" name="">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { reqHasTrademark } from '@/api/product/trademark'
+import type {
+  Records,
+  TradeMarkResponseData,
+} from '@/api/product/trademark/type'
 // 当前页码
 let pageNo = ref<number>(1)
 // 每一页显示多少条数据
-let pageSize = ref<number>(3)
+let limit = ref<number>(3)
+// 存储已有品牌的数据的总数
+let total = ref<number>(0)
+// 存储已有品牌的数组
+let tradenarkArr = ref<Records>([])
+// 获取已有品牌的接口封装成一个函数：在任何情况下调用次函数即可
+const getHasTrademark = async (pager = 1) => {
+  pageNo.value = pager
+  let result: TradeMarkResponseData = await reqHasTrademark(
+    pageNo.value,
+    limit.value,
+  )
+  if (result.code == 200) {
+    // 存储已有品牌总个数
+    total.value = result.data.total
+    tradenarkArr.value = result.data.records
+  }
+}
+// 组件挂在完毕钩子--发送一次请求，获取第一页，一页三个已有的品牌数据
+onMounted(() => {
+  getHasTrademark()
+})
+
+// //分页器当前页码发生变化时出发
+// const changePageNo = () => {
+//   // 当前页码发生变化，再次调用获取当前页的数据
+//   getHasTrademark()
+// }
+
+// 当下拉菜单发生变化的时候【当前页数据改变】
+// const sizeChange = () => {
+//   // 当前页码改变时，页码置为一
+//   pageNo.value = 1
+//   getHasTrademark()
+// }
 </script>
+getHasTrademark
 <style scoped></style>
