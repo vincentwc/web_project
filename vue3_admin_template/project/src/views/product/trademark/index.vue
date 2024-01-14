cd3f65a52487e07f10110829f73a496fb1bd6746
wang_cheng <wang_cheng@cestc.cn>
2024-01-14 19:27:24 +0800

    提交

--- vue3_admin_template/project/src/views/product/trademark/index.vue
+++ vue3_admin_template/project/src/views/product/trademark/index.vue
@@ -2,7 +2,12 @@
   <div class="">
     <el-card class="box-card">
       <!-- 卡片顶部添加品牌按钮 -->
-      <el-button type="primary" size="default" icon="Plus" @click="">
+      <el-button
+        type="primary"
+        size="default"
+        icon="Plus"
+        @click="addTradeMark"
+      >
         添加品牌
       </el-button>
       <!-- 表格组件，用于展示品牌已有的数据 -->
@@ -30,7 +35,12 @@
         </el-table-column>
         <el-table-column label="品牌操作">
           <template #="{ row, $index }">
-            <el-button type="warning" icon="Edit" size="small" />
+            <el-button
+              type="warning"
+              icon="Edit"
+              size="small"
+              @click="updateTradeMark"
+            />
             <el-button type="danger" icon="Delete" size="small" />
           </template>
         </el-table-column>
@@ -47,7 +57,7 @@
        -->
       <el-pagination
         @current-change="getHasTrademark"
-        @size-change="sizeChange"
+        @size-change="getHasTrademark"
         v-model:current-page="pageNo"
         v-model:page-size="limit"
         :page-sizes="[3, 5, 7, 9]"
@@ -56,6 +66,37 @@
         :total="total"
       />
     </el-card>
+    <!-- 对话框组件：在添加品牌与修改已有品牌的业务时候使用结构 -->
+    <!--  
+      v-model：属性用于控制对话框的显示与隐藏 true-显示 false-隐藏
+      title：设置对话框左上角标题
+    -->
+    <el-dialog v-model="dialogFormVisible" title="添加品牌">
+      <el-form style="width: 80%">
+        <el-form-item label="品牌名称" label-width="90px">
+          <el-input placeholder="请输入品牌名称"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="90px">
+          <el-upload
+            class="avatar-uploader"
+            action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <!-- 具名插槽：footer -->
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button type="primary" @click="cancel">取消</el-button>
+          <el-button type="primary" @click="confirm">确定</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script setup lang="ts" name="">
@@ -73,6 +114,8 @@ let limit = ref<number>(3)
 let total = ref<number>(0)
 // 存储已有品牌的数组
 let tradenarkArr = ref<Records>([])
+// 控制对话框显示｜隐藏
+let dialogFormVisible = ref(false)
 // 获取已有品牌的接口封装成一个函数：在任何情况下调用次函数即可
 const getHasTrademark = async (pager = 1) => {
   pageNo.value = pager
@@ -91,18 +134,45 @@ onMounted(() => {
   getHasTrademark()
 })
 
-// //分页器当前页码发生变化时出发
-// const changePageNo = () => {
-//   // 当前页码发生变化，再次调用获取当前页的数据
-//   getHasTrademark()
-// }
+// 添加品牌按钮回调
+const addTradeMark = () => {
+  // 对话框显示
+  dialogFormVisible.value = true
+}
 
-// 当下拉菜单发生变化的时候【当前页数据改变】
-// const sizeChange = () => {
-//   // 当前页码改变时，页码置为一
-//   pageNo.value = 1
-//   getHasTrademark()
-// }
+const updateTradeMark = () => {
+  dialogFormVisible.value = true
+}
+
+// 对话框底部取消按钮
+const cancel = () => {
+  // 对话框隐藏
+  dialogFormVisible.value = false
+}
+
+const confirm = () => {
+  dialogFormVisible.value = false
+}
 </script>
-getHasTrademark
-<style scoped></style>
+<style scoped>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+</style>
