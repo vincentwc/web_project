d2bd264a6d6c0cf4e2c39f0722f48758a1724803
wang_cheng <wang_cheng@cestc.cn>
2024-01-22 14:36:38 +0800

    SPU静态页面搭建

--- vue3_admin_template/project/src/views/product/sku/index.vue
+++ vue3_admin_template/project/src/views/product/sku/index.vue
@@ -1,9 +1,7 @@
 <template>
- <div class=''>
-  <h1>sku管理</h1>
- </div>
+  <div class="">
+    <h1>sku管理</h1>
+  </div>
 </template>
-<script setup lang='ts' name=''>
-</script>
-<style scoped>
-</style>
+<script setup lang="ts" name=""></script>
+<style scoped></style>
--- vue3_admin_template/project/src/views/product/spu/index.vue
+++ vue3_admin_template/project/src/views/product/spu/index.vue
@@ -1,9 +1,40 @@
 <template>
- <div class=''>
-  <h1>spu管理</h1>
- </div>
+  <div class="">
+    <!-- 三级分类全局组件 -->
+    <Category :scene="scene" />
+    <!--  -->
+    <el-card style="margin: 10px 0px">
+      <el-button type="primary" size="default" icon="Plus">添加SPU</el-button>
+      <el-table style="margin: 10px 0px" border>
+        <el-table-column
+          label="序号"
+          type="index"
+          align="cunter"
+          width="80px"
+        ></el-table-column>
+        <el-table-column label="SPU名称"></el-table-column>
+        <el-table-column label="SPU描述"></el-table-column>
+        <el-table-column label="SPU操作"></el-table-column>
+      </el-table>
+      <!-- 分页器 -->
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 5, 7, 9]"
+        :background="true"
+        layout=" prev, pager, next, jumper,->, sizes, total"
+        :total="400"
+      />
+    </el-card>
+  </div>
 </template>
-<script setup lang='ts' name=''>
+<script setup lang="ts" name="">
+import { ref } from 'vue'
+// 场景数据
+const scene = ref<number>(0)
+// 分页器默认页码
+let pageNo = ref<number>(1)
+// 每一页展示几条数据
+let pageSize = ref<number>(3)
 </script>
-<style scoped>
-</style>
+<style scoped></style>
